80c6b0bcc352d89f015592287a014812d19a3afc
msovet <merei9822@gmail.com>
2021-02-18 16:34:04 +0600

    Initial commit

---
+++ src/test/java/kz/msovet/appkazdream/AppKazdreamApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.msovet.appkazdream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKazdreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
