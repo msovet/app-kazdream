85f665459d382e9b73c7cf6b5ddd1a0fb0286cdc
msovet <merei9822@gmail.com>
2021-02-18 18:48:29 +0600

    models

---
+++ src/main/java/kz/msovet/appkazdream/model/Comment.java
@@ -0,0 +1,13 @@
+package kz.msovet.appkazdream.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
